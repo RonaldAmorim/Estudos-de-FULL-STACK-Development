9c0f0e533fd793ebbc54e3bb6313eeb104bc1be2
Ronald @morim <amorim.ronald@gmail.com>
2021-08-25 08:55:41 -0300

    Fazendo exercícios
    Estudados os capítulos 1 até 3 do livro, HTML5 e CSS3 Domine a web do futuro

---
+++ curso-JavaScript/Experimentos/script.js
@@ -0,0 +1,8 @@
+function testando() {
+    let somando = document.querySelector('.div-1-1 #span1')
+    let multiplicando = document.querySelector('.div-1-1 #span2')
+  
+    somando.innerHTML = 'COISANS'
+    multiplicando.innerHTML = 'WWWWWWW'
+  }
+    
